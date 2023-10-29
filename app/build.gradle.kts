19c868d0bb4d16346c7131ba8f1f7ffbf9703a2d
toniviu99 <toniviugutierrez@gmail.com>
2023-10-29 09:26:40 +0100

    DaggerHilt

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,8 @@
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
+    id("kotlin-kapt")
+    id("com.google.dagger.hilt.android")
 }
 
 android {
@@ -36,9 +38,21 @@ android {
     buildFeatures{
         viewBinding = true
     }
+    //this for DaggerHilt
+    kotlin {
+        jvmToolchain(8)
+    }
 }
 
 dependencies {
+
+
+    //DaggerHilt
+    implementation("com.google.dagger:hilt-android:2.48")
+    kapt("com.google.dagger:hilt-compiler:2.48")
+
+    //ViewModel
+    implementation ("androidx.fragment:fragment-ktx:1.6.1")
 
     implementation("androidx.core:core-ktx:1.9.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
--- app/src/main/java/com/example/spaceapp/ui/home/MainActivity.kt
+++ app/src/main/java/com/example/spaceapp/ui/home/MainActivity.kt
@@ -2,10 +2,15 @@ package com.example.spaceapp.ui.home
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.viewModels
 import com.example.spaceapp.R
 import com.example.spaceapp.databinding.ActivityMainBinding
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+
+    private val mainViewModel by viewModels <MainViewModel>()
 
     private lateinit var binding: ActivityMainBinding
 
